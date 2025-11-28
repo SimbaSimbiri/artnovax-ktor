717a7b0b1a199f7e2ebc88cc873991739f15dc6a
SimbaSimbiri <simbiriisaacray@gmail.com>
2025-11-28 16:12:35 +0300

    Changed logback classic version to prevent malicious attack

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,8 @@ dependencies {
     implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
+    implementation(libs.ktor.server.call.logging)
+
     // aws rds specific
     implementation(libs.postgres.driver)
     implementation(libs.hikari)
